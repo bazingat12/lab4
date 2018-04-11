43799f90aacaacb44f440c9267c04e99ae5c603e
Tanya <t.a.kraevaa@mail.com>
2018-04-11 17:35:08 +0300

    Добавлены классы

--- lab4/lab4/Program.cs
+++ lab4/lab4/Program.cs
@@ -10,7 +10,14 @@ namespace lab4
     {
         static void Main(string[] args)
         {
-
+            Book Book = new Book(1, "Пушкин", "Руслан и Людмила", "Поэма");
+            Exemplar exemplar = new Exemplar(1, Book, DateTime.Now, "Exmo");
+            Position position = new Position(1, "Библиотекарь");
+            Reader reader = new Reader(1, "Семенов", "Игорь", "Евгеньевич", "муж.", 79835);
+            Employee employee = new Employee(1, position, "Петрова", "Людмила", "Сергеевна", 345678892, 4);
+            Delivery delivery = new Delivery(1, DateTime.Now, employee, reader, exemplar);
+            delivery.Info();
+            Console.ReadLine();
         }
     }
 }
